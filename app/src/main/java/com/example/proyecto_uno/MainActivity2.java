79cc98b09a5b8fa8f0d1900b91d1e71b5d20d8fe
Yeferson <y.a.berdugo@gmail.com>
2022-12-17 23:41:05 -0500

    Ajuste productos, se agregan dependencias al codigo

--- app/src/main/java/com/example/proyecto_uno/MainActivity2.java
+++ app/src/main/java/com/example/proyecto_uno/MainActivity2.java
@@ -36,6 +36,7 @@ public class MainActivity2 extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
         botonproducto2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
